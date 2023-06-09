4869b6dae50b05ff5dfee940a5e90b83808ad16a
Allan Fortes Barbosa <53353680+TDCAS@users.noreply.github.com>
2023-06-09 14:39:35 -0300

    Add files via upload

---
+++ 76PraticandocomFactoryfunction/assets/js/main.js
@@ -0,0 +1,36 @@
+//variaveis
+const buttons = document.querySelectorAll("#button")
+const buttonClear = document.getElementById("buttonClear")
+const buttonResult = document.getElementById("buttonResult")
+let input = document.getElementById("inputView")
+const calculator = calculatorReal()
+
+//eventos
+calculator.start()
+//funcao que fazeram as tarefas
+
+function calculatorReal(){
+      return {
+            start(){
+                  return this.clickButtons()
+            },
+            clickButtons(){
+                  document.addEventListener("click",function(event){
+                        const arrayButton = Array.from(buttons)
+                        if(arrayButton.indexOf(event.target) !== -1) return calculator.displayView(event.target.textContent)
+                        else if(event.target === buttonClear) return calculator.inputClear()
+                        else if(event.target === buttonResult) return calculator.result(arrayButton)
+                  })
+
+            },
+            displayView(newButton){
+                  return input.value += newButton
+            },
+            inputClear(){
+                  return input.value = " "
+            },
+            result(arrayButton){
+                  console.log(eval(input.value))     
+            }
+      }
+}
